0c4fa3109d5c842ff8ba971665079fd1ddf5ac00
Joe Warner <jwarner@amplience.com>
2021-04-30 23:24:21 +0100

    feat(BERRY-2): Twit configured correctly

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const twit = new Twit({
   consumer_secret: process.env.CONSUMER_SECRET,
   access_token: process.env.ACCESS_TOKEN,
   access_token_secret: process.env.ACCESS_SECRET,
+  timeout_ms: 60 * 1000,
+  strictSSL: true,
 });
 
-await twit.post("statuses/update", { status: `` });
+await twit.post("statuses/update", { status: `hehee matt smell` });
 
 console.log(`Done annoying Matt Berry 🐛`);
